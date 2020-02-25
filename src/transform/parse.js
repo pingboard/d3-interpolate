54139c931e3f5466fe12da0a9fbd61b9e80dbf1c
Ryan Schutte <ryan@pingboard.com>
2020-02-24 21:28:07 -0600

    adds better IE support

--- src/transform/parse.js
+++ src/transform/parse.js
@@ -1,15 +1,22 @@
-import decompose, {identity} from "./decompose";
+import decompose, { identity } from "./decompose";
 
 var svgNode;
 
 export function parseCss(value) {
-  const m = new (typeof DOMMatrix === "function" ? DOMMatrix : WebKitCSSMatrix)(value + "");
+  const Matrix =
+    typeof DOMMatrix === "function"
+      ? DOMMatrix
+      : typeof WebKitCSSMatrix === "function"
+      ? WebKitCSSMatrix
+      : MSCSSMatrix;
+  const m = new Matrix(value + "");
   return m.isIdentity ? identity : decompose(m.a, m.b, m.c, m.d, m.e, m.f);
 }
 
 export function parseSvg(value) {
   if (value == null) return identity;
-  if (!svgNode) svgNode = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  if (!svgNode)
+    svgNode = document.createElementNS("http://www.w3.org/2000/svg", "g");
   svgNode.setAttribute("transform", value);
   if (!(value = svgNode.transform.baseVal.consolidate())) return identity;
   value = value.matrix;
